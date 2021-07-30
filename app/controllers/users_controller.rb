08c4c8bd319e7a086b60a3552bf889ec22374761
Damian Malysza <damian.malysza@gmail.com>
2021-07-29 21:32:43 -0400

    created basic login form for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    #need logic for how to handle OAuth
+    #need logic and alerting if username already exists
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to '/' #change this redirect eventually
   end
 
   def new
+    @user = User.new
   end
 
   def edit
@@ -18,5 +24,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password)
   end
 end
