4a6219258baa2bb9f855609fded1338902f2171c
Damian Malysza <damian.malysza@gmail.com>
2021-08-18 20:12:45 -0400

    added logic for logging user in and creating a session and added helper methods for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,21 @@ class SessionsController < ApplicationController
   end
   
   def login
-    @user = User.new
   end
 
   def create
-    binding.pry
+    user = User.find_by(username: params[:username])
+    #add logic for if a user isn't find
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      #add error message if an incorrect password is provided
+      redirect_to login_path
+    end
   end
 
   def logout
+    #add logic for logging out a user
   end
 end
