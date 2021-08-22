049991aed56eb60eb7d1848fb033129a558454b7
Damian Malysza <damian.malysza@gmail.com>
2021-08-22 13:00:18 -0400

    reverted user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    #need logic for how to handle OAuth
     #need logic and alerting if username already exists
-    binding.pry
-    user = User.find_or_create_by(user_params)
+    user = User.create(user_params)
     session[:user_id] = user.id
     redirect_to user_path(user)
   end
