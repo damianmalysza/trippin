78a35c9136b22c5384a3e34db32eb59e29b974d6
Damian Malysza <damian.malysza@gmail.com>
2021-08-22 16:37:04 -0400

    changed user new and edit forms to partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
   end
-
+  
   def create
     if form_fields_completed?
       user ||= User.find_by(username: params[:user][:username])
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
         redirect_to user_path(user)
       end
     end
-
+    
   end
-
+  
   def new
     if logged_in?
       redirect_to user_path(current_user)
@@ -23,33 +23,42 @@ class UsersController < ApplicationController
       @user = User.new
     end
   end
-
+  
   def edit
     @user = User.find(params[:id])
     if current_user != @user
       redirect_to user_path(current_user)
     end
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
-
+  
   def update
+    user = User.find(params[:id])
+    new_name = params[:user][:username]
+    if User.find_by(username: new_name)
+      redirect_to edit_user_path(user), notice: "Username already exists - please try another one"
+    else
+      user.update(username: params[:user][:username])
+      user.save
+      redirect_to user_path(user)
+    end 
   end
-
+  
   def destroy
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:username,:password)
   end
-
+  
   def form_fields_completed?
-      !params[:user][:username].nil? && !params[:user][:password].nil?
+    !params[:user][:username].nil? && !params[:user][:password].nil?
   end
-
+  
   
 end
