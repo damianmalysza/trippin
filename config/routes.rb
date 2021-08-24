4f2973fc3cb2056adf2f53a80391d9db6110c795
Damian Malysza <damian.malysza@gmail.com>
2021-08-24 19:29:14 -0400

    added skeleton for trips controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "sessions#welcome"
   
   resources :users
+  resources :trips
   get '/', to: 'sessions#welcome'
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
