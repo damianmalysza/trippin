1dc28df8403cb45ab423ec8321db3ab2ae69496f
Damian Malysza <damian.malysza@gmail.com>
2021-09-01 22:35:25 -0400

    did additional styling on create trip form

--- app/javascript/packs/app-globals.js
+++ app/javascript/packs/app-globals.js
@@ -1,10 +1,20 @@
-window.isNumberKey = function(evt)
-{
-   var charCode = (evt.which) ? evt.which : event.keyCode
-   if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-       alert("Please Enter Only Numeric Value:");
-       return false;
-   }
-
-   return true;
-}
+window.isNumberKey = function(evt, element) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && !(charCode == 46 || charCode == 8))
+      return false;
+    else {
+      var len = $(element).val().length;
+      var index = $(element).val().indexOf('.');
+      if (index > 0 && charCode == 46) {
+        return false;
+      }
+      if (index > 0) {
+        var CharAfterdot = (len + 1) - index;
+        if (CharAfterdot > 3) {
+          return false;
+        }
+      }
+  
+    }
+    return true;
+  }
