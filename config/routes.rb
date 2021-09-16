5af32523c385c8cd321b6d12eaff3fbdb3f231ec
Damian Malysza <damian.malysza@gmail.com>
2021-09-16 12:03:27 -0400

    added activity and post controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
   root to: "sessions#welcome"
   
   resources :users 
