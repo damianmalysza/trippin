632de2fe9fd369dfeb37b9a06e3ba92074ae6928
Damian Malysza <damian.malysza@gmail.com>
2021-09-21 19:11:21 -0400

    added new comment form and appropriate links to post show page for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
   def new
+    @trip = Trip.find(params[:trip_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
 
   def create
+    binding.pry
+    post = Post.find(params[:post_id])
+    comment = Comment.create(comment_params)
+    comment.user = current_user
+    post.comments << comment 
+    redirect_to trip_post_path(post.trip, post)
   end
 
   def edit
@@ -12,5 +21,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
