358801b171c4263335b8a268e35686cdf80ca426
Damian Malysza <damian.malysza@gmail.com>
2021-09-20 21:48:39 -0400

    added post actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,44 @@ class PostsController < ApplicationController
   end
 
   def new
+    @trip = Trip.find(params[:trip_id])
+    @post = Post.new
   end
 
   def create
+    trip = Trip.find(params[:trip_id])
+    post = Post.create(post_params)
+    post.user = current_user
+    trip.posts << post
+    redirect_to trip_path(trip)
   end
 
   def show
+    @trip = Trip.find(params[:trip_id])
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @trip = Trip.find(params[:trip_id])
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to trip_post_path(post.trip,post)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    trip = post.trip
+    post.destroy
+    redirect_to trip_path(trip)
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
