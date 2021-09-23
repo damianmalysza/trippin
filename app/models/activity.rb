13196b11c2157b80472ab442c020cbc27a3cd1d1
Damian Malysza <damian.malysza@gmail.com>
2021-09-22 22:14:57 -0400

    accounted for if an activity is made with a trip but no date is given on activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,20 +1,22 @@
 class Activity < ApplicationRecord
-
+  
   belongs_to :trip
   has_many :user_activities
   has_many :users, through: :user_activities
-
+  
   validates :name, presence: true
   validate :valid_date?
-
+  
   def act_date
     self.date.nil? ? "Date TBD" : self.date.strftime("%b. %d, %Y")
   end 
-
+  
   def valid_date?
-    trip = self.trip
-    unless date > trip.start_date && date < trip.end_date
-      errors.add(:date, "must be during trip")
+    if date
+      trip = self.trip
+      unless date > trip.start_date && date < trip.end_date
+        errors.add(:date, "must be during trip")
+      end
     end
   end
 end
