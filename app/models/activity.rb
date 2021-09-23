409dcc95a6352ea9e6a7d88b8bcc443a44804f1e
Damian Malysza <damian.malysza@gmail.com>
2021-09-22 22:03:32 -0400

    added date validation for activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,9 +5,16 @@ class Activity < ApplicationRecord
   has_many :users, through: :user_activities
 
   validates :name, presence: true
+  validate :valid_date?
 
   def act_date
     self.date.nil? ? "Date TBD" : self.date.strftime("%b. %d, %Y")
   end 
 
+  def valid_date?
+    trip = self.trip
+    unless date > trip.start_date && date < trip.end_date
+      errors.add(:date, "must be during trip")
+    end
+  end
 end
