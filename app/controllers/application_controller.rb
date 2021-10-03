978053520f25eb2d35ab6a7b9c1e9ca378c16262
Damian Malysza <damian.malysza@gmail.com>
2021-10-03 16:45:16 -0400

    added requirement for use to be logged in to use the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
   
+  before_action :require_login
+
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to login_path, notice: "Please log in to use the app"
+    end
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :require_login
+
   def welcome
     redirect_to user_path(current_user) if logged_in?
   end
